cc29824620abc073c48ae904b5a53904aa318430
AstLey32 <160470174+AstLey32@users.noreply.github.com>
2025-06-06 10:14:43 +0800

    update ua

--- local_translate/local_translate/search.py
+++ local_translate/local_translate/search.py
@@ -12,7 +12,7 @@ def get_keywords():
 def weibo():
     url = "https://s.weibo.com/top/summary?cate=realtimehot"
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
         'Host': 's.weibo.com',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
